ab84a973d3a37645f83df17eec9979a708a2089f
hwanghero <hpky123@naver.com>
2020-07-18 00:22:15 +0900

    차트

---
+++ insaProjecct_v2/insaState/dept_State.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace insaProjecct_v2
+{
+    public partial class dept_State : Form
+    {
+        public dept_State()
+        {
+            InitializeComponent();
+        }
+    }
+}
